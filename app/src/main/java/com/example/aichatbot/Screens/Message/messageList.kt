5ea9b403c98284b2ef782df76f4e2e70d017359f
Abbas1608 <shaikhmohdabbas07@gmail.com>
2025-08-06 21:18:02 +0530

    message

--- app/src/main/java/com/example/aichatbot/Screens/Message/messageList.kt
+++ app/src/main/java/com/example/aichatbot/Screens/Message/messageList.kt
@@ -1,19 +1,25 @@
 package com.example.aichatbot.Screens.Message
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.aichatbot.R
@@ -23,14 +29,30 @@ import com.example.aichatbot.R
 fun messageList(modifier: Modifier,messList: List<messageModel>)
 {
 
-    LazyColumn(
-        modifier = modifier,
-        reverseLayout = true
-    ) {
-        items(messList.reversed()){
-            messageListUI(it)
+    if (messList.isEmpty()){
+        Column(modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text(text = "Where should we begin?",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                color = colorResource(R.color.white))
         }
     }
+    else{
+        LazyColumn(
+            modifier = modifier,
+            reverseLayout = true
+        ) {
+            items(messList.reversed()){
+                messageListUI(it)
+            }
+        }
+    }
+
+
 }
 
 @Composable
@@ -45,13 +67,16 @@ fun messageListUI(
         Box(modifier = Modifier.fillMaxWidth()){
             //AI
             if (isModel){
-                Text(
-                    text = messageModel.message,
-                    fontSize = 14.sp,
-                    color = colorResource(R.color.white),
-                    modifier = Modifier
-                        .padding(end = 25.dp, bottom = 5.dp, top = 5.dp)
-                )
+                SelectionContainer {
+                    Text(
+                        text = messageModel.message,
+                        fontSize = 14.sp,
+                        color = colorResource(R.color.white),
+                        modifier = Modifier
+                            .padding(end = 25.dp, bottom = 5.dp, top = 5.dp)
+                    )
+                }
+
             }
             //users
             else {
@@ -66,13 +91,16 @@ fun messageListUI(
                         )
                         .align (alignment = Alignment.BottomEnd)
                 ) {
-                    Text(
-                        text = messageModel.message,
-                        fontSize = 14.sp,
-                        color = colorResource(R.color.white),
-                        modifier = Modifier
-                            .padding(10.dp)
-                    )
+                    SelectionContainer {
+                        Text(
+                            text = messageModel.message,
+                            fontSize = 14.sp,
+                            color = colorResource(R.color.white),
+                            modifier = Modifier
+                                .padding(10.dp)
+                        )
+                    }
+
                 }
             }
         }
